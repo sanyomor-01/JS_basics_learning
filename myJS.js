5c91e43144ce8258ce7eb39c8d55c926a9b41c73
sanyomor-01 <sanyomormichael@gmail.com>
2023-10-03 01:46:33 +0000

    .reduce()

--- myJS.js
+++ myJS.js
@@ -338,3 +338,15 @@ const startsWithS = Animals.findIndex(animal => {
   return animal[0] === 's';
 });
 
+//The .reduce() Method
+//Another widely used iteration method is .reduce(). The .reduce() method returns a single value after iterating through the elements of an array, thereby reducing the array.
+
+const newNumbers = [1, 3, 5, 7];
+
+const newSum = newNumbers.reduce((accumulator, currentValue) => {
+  console.log('The value of accumulator: ', accumulator);
+  console.log('The value of currentValue: ', currentValue);
+  return accumulator + currentValue;
+}, 10);
+
+console.log(newSum);
