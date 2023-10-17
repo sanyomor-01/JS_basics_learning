698255cd00db0501f64fd39cbfa15b48aedb4437
sanyomor-01 <sanyomormichael@gmail.com>
2023-10-17 16:42:18 +0000

    getters and dealing with arrow functions

--- myJS.js
+++ myJS.js
@@ -461,3 +461,19 @@ const robot = {
   }
 };
 console.log(robot.provideInfo())
+
+const cop = {
+  _model: '1E78V2',
+  _energyLevel: 100,
+  get energyLevel(){
+    if(typeof this._energyLevel === 'number') {
+      return 'My current energy level is ' + this._energyLevel
+    }
+    else {
+      return 'System malfunction: cannot retrieve energy level'
+    }
+  }
+};
+console.log(cop.energyLevel)
+
+
