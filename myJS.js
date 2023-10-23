7642e9dc5ef43d7173b2d0fba09e2e72cc192960
sanyomor-01 <sanyomormichael@gmail.com>
2023-10-23 15:21:52 +0000

    object instances of surgeon class

--- myJS.js
+++ myJS.js
@@ -517,3 +517,12 @@ class Surgeon {
     this.department = department
   }
 }
+//object instances of surgeon class
+class Surgeon {
+  constructor(name, department) {
+    this.name = name;
+    this.department = department;
+  }
+}
+const surgeonRomero = new Surgeon('Francisco Romero','Cardiovascular')
+const surgeonJackson = new Surgeon('Ruth Jackson', 'Orthopedics')
