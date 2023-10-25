4e4d8940504d00503e964d18fb867a70b64023c6
sanyomor-01 <sanyomormichael@gmail.com>
2023-10-25 00:21:46 +0000

    inheritance

--- myJS.js
+++ myJS.js
@@ -541,3 +541,32 @@ const surgeonJackson = new Surgeon('Ruth Jackson', 'Orthopedics');
 console.log(surgeonRomero.name)
 surgeonRomero.takeVacationDays(3)
 console.log(surgeonRomero.remainingVacationDays)
+
+//inheritance
+class HospitalEmployee {
+  constructor(name) {
+    this._name = name;
+    this._remainingVacationDays = 20;
+  }
+  
+  get name() {
+    return this._name;
+  }
+  
+  get remainingVacationDays() {
+    return this._remainingVacationDays;
+  }
+  
+  takeVacationDays(daysOff) {
+    this._remainingVacationDays -= daysOff;
+  }
+}
+
+class Nurse extends HospitalEmployee {
+  constructor(name, certifications) {
+    super(name);
+    this._certifications = certifications;
+  }
+}
+
+const nurseOlynyk = new Nurse('Olynyk', ['Trauma', 'Pediatrics']);
