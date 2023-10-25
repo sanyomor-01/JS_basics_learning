f0dbf04df5d9e601324959fea36cf8a655f6d3cd
sanyomor-01 <sanyomormichael@gmail.com>
2023-10-25 17:07:03 +0000

    module functions

--- secret image/main.js
+++ secret image/main.js
@@ -1,15 +1,8 @@
+import { toggleHiddenElement } from '../modules/dom-functions.js';
 const buttonElement = document.getElementById('secret-button');
 const imgElement = document.getElementById('secret-image');
 
-console.log(imgElement)
-console.log(buttonElement)
-const toggleHiddenElement = (domElement) => {
-    if (domElement.style.display === 'none') {
-      domElement.style.display = 'block';
-    } else {
-      domElement.style.display = 'none';
-    }
-}
+
 
 buttonElement.addEventListener('click', () => {
   toggleHiddenElement(imgElement);
