d5fb0e5368c6fc03fb956aefcc5c8411e075e76f
sanyomor-01 <sanyomormichael@gmail.com>
2023-10-26 20:29:31 +0000

    modules imported

--- message/JS.js
+++ message/JS.js
@@ -1,13 +1,9 @@
+import { toggleHiddenElement, changeToFunkyColor } from '../modules/dom-functions.js';
+
 const buttonElement = document.getElementById('btn');
 const pElement = document.getElementById('secret-p');
 
-const toggleElement = (domElement) =>{
-    if (domElement.style.display === 'none'){
-        domElement.style.display = 'block';
-    }
-    else{
-        domElement.style.display = 'none'
-    }
-
-}
-buttonElement.addEventListener( 'click', () =>{toggleElement(pElement)})
+buttonElement.addEventListener('click', () => {
+  toggleHiddenElement(pElement);
+  changeToFunkyColor(buttonElement);
+});
